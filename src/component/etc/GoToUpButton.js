e4c0a8457ac5f00e078d36c1518dabd1766226b6
Anchovia <hodolye@naver.com>
2023-01-24 03:40:02 +0900

    상단 이동 버튼 기능 추가

---
+++ src/component/etc/GoToUpButton.js
@@ -0,0 +1,22 @@
+import styles from "./GoToUpButton.module.css"
+
+import iconGoToUp from '../../images/icon/image_icon_goToUp.png'
+
+export default function GoToUpButton(){
+    function onClick(){
+        if(!window.scrollY){
+            return;
+        }
+
+        window.scrollTo({
+            top : 0,
+            behavior : 'smooth'
+        });
+    };
+
+    return (
+        <div>
+            <img src={iconGoToUp} alt="IconGoToUp" onClick={onClick} className={styles.button}/>
+        </div>
+    );
+}
