c12398a0d4f7bad82818dec75b46808b762d602c
Anchovia <hodolye@naver.com>
2023-03-16 13:42:39 +0900

    css 디자인 전체적으로 수정

--- src/component/display/MembersOnlyWindow.js
+++ src/component/display/MembersOnlyWindow.js
@@ -11,28 +11,26 @@ export default function MembersOnlyWindow({setGetToken}){
     }
 
     return(
-        <div className={styles.container}>
-            <div className={styles.gridTable}>
-                <div className={styles.image}/>
-                <div className={styles.element}>
-                    <div className={styles.topContainer}>
-                        <div className={styles.nickName}>닉네임</div>
-                        <button className={styles.buttonLogout} onClick={logout}>로그아웃</button>
-                    </div>
-                    <div className={styles.email}>test@123.com</div>
-                    <div className={styles.userData}>
-                        <Link className={styles.link}>
-                            <span className={styles.linkTitle}>게시글</span>
-                            <span className={styles.linkElement}>9999+</span>
-                        </Link>
-                        <Link className={styles.link}>
-                            <span className={styles.linkTitle}>댓글</span>
-                            <span className={styles.linkElement}>9999+</span>
-                        </Link>
-                    </div>
+        <div className={styles.gridTable}>
+            <div className={styles.image}/>
+            <div className={styles.element}>
+                <div className={styles.topContainer}>
+                    <div className={styles.nickName}>닉네임</div>
+                    <button className={styles.buttonLogout} onClick={logout}>로그아웃</button>
                 </div>
-                <button className={styles.buttonEditProfile}>프로필 수정</button>
+                <div className={styles.email}>test@123.com</div>
+                <div className={styles.userData}>
+                    <Link className={styles.link}>
+                        <span className={styles.linkTitle}>게시글</span>
+                        <span className={styles.linkElement}>9999+</span>
+                    </Link>
+                    <Link className={styles.link}>
+                        <span className={styles.linkTitle}>댓글</span>
+                        <span className={styles.linkElement}>9999+</span>
+                    </Link>
+                </div>
             </div>
+            <button className={styles.buttonEditProfile}>프로필 수정</button>
         </div>
     );
 };
