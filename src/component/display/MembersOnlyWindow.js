4fb551620b125a3ba601b9b60252dca19fe80c3f
Anchovia <hodolye@naver.com>
2023-03-16 17:12:33 +0900

    display info 비동기 처리 및 유저데이터 연동

--- src/component/display/MembersOnlyWindow.js
+++ src/component/display/MembersOnlyWindow.js
@@ -1,24 +1,50 @@
 // 모듈 import
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 // CSS import
 import styles from "./MembersOnlyWindow.module.css";
 
 export default function MembersOnlyWindow({setGetToken}){
+    const url = "http://localhost:8080/info"
+
+    const [userEmail, setUserEmail] = useState("");
+    const [userName, setUserName] = useState("");
+
     function logout(e){
         e.preventDefault();
         setGetToken(true);
     }
+
+    let useUserData = async(url) => {
+        try{
+            const res = await axios.get(url, {
+                headers: {
+                    "content-type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem("jwt"),
+                }
+            });
+
+            setUserEmail(res.data.memberEmail)
+            setUserName(res.data.memberName)
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    useUserData(url);
 
     return(
         <div className={styles.gridTable}>
             <div className={styles.image}/>
             <div className={styles.element}>
                 <div className={styles.topContainer}>
-                    <div className={styles.nickName}>닉네임</div>
+                    <div className={styles.nickName}>{userName}</div>
                     <button className={styles.buttonLogout} onClick={logout}>로그아웃</button>
                 </div>
-                <div className={styles.email}>test@123.com</div>
+                <div className={styles.email}>{userEmail}</div>
                 <div className={styles.userData}>
                     <Link className={styles.link}>
                         <span className={styles.linkTitle}>게시글</span>
