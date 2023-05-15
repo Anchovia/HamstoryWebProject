16f0785d3a5f7297422510dce39b40b1bd7495cc
Anchovia <hodolye@naver.com>
2023-05-15 09:16:43 +0900

    ComminotyLatestPost 코드 변수 최적화

--- src/component/community/CommunityLatestPost.js
+++ src/component/community/CommunityLatestPost.js
@@ -2,17 +2,17 @@
 import CommunityHeader from './CommunityHeader';
 import PostList from "../post/PostList";
 import Loading from "../Loading";
+import PostingButton from '../button/PostingButton';
 
 // config import
 import { URL_GET_BOARD } from '../../config/config';
 
 // hook import
 import useFetch from '../../hooks/useFetch';
+import useScrollTop from '../../hooks/useScrollTop';
 
 // css import
 import styles from './CommunityLatestPost.module.css';
-import PostingButton from '../button/PostingButton';
-import useScrollTop from '../../hooks/useScrollTop';
 
 export default function CommunityLatestPost(){
     const {data : res, loading} = useFetch(URL_GET_BOARD); // 데이터를 가져오는 hook
@@ -28,6 +28,10 @@ export default function CommunityLatestPost(){
         );
     }
 
+    const renderPostList = ({ id, title, writer, createdTime, hits, likes }) => (
+        <PostList key={id} id={id} title={title} writer={writer} createdTime={createdTime} hits={hits} likes={likes} />
+    );
+
     return(
         <div className={styles.body}>
             <PostingButton/>
@@ -39,11 +43,7 @@ export default function CommunityLatestPost(){
                 <div className={styles.headerContents}>조회</div>
                 <div className={styles.headerContents}>좋아요</div>
             </div>
-                {
-                    posts.map((post, index) => (
-                        <PostList key={post.id} id = {post.id} title = {post.title} writer = {post.writer} createdTime = {post.createdTime} hits = {post.hits} likes = {post.likes} ></PostList>
-                    ))
-                }
+            {posts.map(renderPostList)}
         </div>
     );
 }
